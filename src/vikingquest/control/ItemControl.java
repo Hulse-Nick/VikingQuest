e11ad7d35b11ca7da247bdc78061a4cb1e8902fe
Nicholas <Nicholas@Nick>
2015-02-25 01:10:41 -0700

    Added and removed classes.

---
+++ src/vikingquest/control/ItemControl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vikingquest.control;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class ItemControl {
+    public String[] itemList = new String[40];
+    public String[] equippedList= new String[7];
+    
+//    To be placed into new game for starting gear 
+//    equipedList [0]= "Tattered Helm";
+//    equipedList [1]= "Tattered Breastplate";
+//    equipedList [2]= "Tattered Pants";
+//    equipedList [3]= "Tattered Gloves";
+//    equipedList [4]= "Tattered Boots";
+//    equipedList [5]= "Tattered Sword";
+//    equipedList [6]= "Tattered Shield";
+   
+}
